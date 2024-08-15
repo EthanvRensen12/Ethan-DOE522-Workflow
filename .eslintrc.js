37fe93f85b77564cad4f3103c24c2222da312bc2
EthanvRensen12 <152304606+EthanvRensen12@users.noreply.github.com>
2024-08-15 19:04:41 +0200

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+npm install eslint --save-dev
+
+npx eslint --init
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',  // Use if you are using React
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+  },
+};
+
+npx eslint src/**/*.js
+
+
